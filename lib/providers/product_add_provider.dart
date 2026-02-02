96d3314f3417b09bdb53af194c1e4128f1096c95
thizkavinda25 <thizarakavinda@gmail.com>
2026-02-02 23:13:11 +0530

    Admin State Management setup

--- lib/providers/product_add_provider.dart
+++ lib/providers/product_add_provider.dart
@@ -1,3 +1,36 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_final_fields
 
-class ProductAddProvider extends ChangeNotifier {}
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProductAddProvider extends ChangeNotifier {
+  TextEditingController _productNameController = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
+  TextEditingController _priceController = TextEditingController();
+
+  TextEditingController get productNameController => _productNameController;
+  TextEditingController get descriptionController => _descriptionController;
+  TextEditingController get priceController => _priceController;
+
+  String? _selectedCategoryId;
+  String? get selectedCategoryId => _selectedCategoryId;
+
+  List<File> _pickedImages = [];
+  List<File> get pickedImages => _pickedImages;
+
+  void setProductCategory(String id) {
+    _selectedCategoryId = id;
+    notifyListeners();
+  }
+
+  Future<void> addImages() async {
+    final imagePicker = ImagePicker();
+    final images = await imagePicker.pickMultiImage(
+      limit: 5,
+      imageQuality: 70,
+      maxHeight: 600,
+    );
+    _pickedImages = images.map((e) => File(e.path)).toList();
+  }
+}
--- lib/screens/admin/product_add_screen.dart
+++ lib/screens/admin/product_add_screen.dart
@@ -1,6 +1,8 @@
 import 'package:abans_online/components/custom_text_field.dart';
 import 'package:abans_online/data/categories.dart';
+import 'package:abans_online/providers/product_add_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProductAddScreen extends StatefulWidget {
   const ProductAddScreen({super.key});
@@ -12,60 +14,85 @@ class ProductAddScreen extends StatefulWidget {
 class _ProductAddScreenState extends State<ProductAddScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.grey.shade900,
-        foregroundColor: Colors.white,
-        centerTitle: true,
-        title: Text('Add Product'),
-      ),
-      body: Padding(
-        padding: EdgeInsetsGeometry.symmetric(vertical: 12, horizontal: 16),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CustomTextField(labelText: 'Name'),
-              CustomTextField(
-                labelText: 'Description',
-                maxLines: 5,
-                maxLength: 50,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: Text('Product Category'),
-              ),
-              Wrap(
-                children: Categories.list.map((category) {
-                  return Padding(
-                    padding: const EdgeInsets.only(right: 8, bottom: 15),
-                    child: Chip(
-                      avatar: Icon(category.icon),
-                      label: Text(category.name),
-                    ),
-                  );
-                }).toList(),
-              ),
-              CustomTextField(
-                labelText: 'Price',
-                keyboardType: TextInputType.number,
-                prefixText: 'LKR ',
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Product Category'),
-                    FilledButton(onPressed: () {}, child: Text('Add Images')),
-                  ],
-                ),
-              ),
-            ],
+    return Consumer<ProductAddProvider>(
+      builder: (context, productAddProvider, child) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.grey.shade900,
+            foregroundColor: Colors.white,
+            centerTitle: true,
+            title: Text('Add Product'),
           ),
-        ),
-      ),
+          body: Padding(
+            padding: EdgeInsetsGeometry.symmetric(vertical: 12, horizontal: 16),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomTextField(
+                    labelText: 'Name',
+                    controller: productAddProvider.productNameController,
+                  ),
+                  CustomTextField(
+                    labelText: 'Description',
+                    maxLines: 5,
+                    maxLength: 50,
+                    controller: productAddProvider.descriptionController,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text('Product Category'),
+                  ),
+                  Wrap(
+                    children: Categories.list.map((category) {
+                      final isSelected =
+                          productAddProvider.selectedCategoryId == category.id;
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 8, bottom: 15),
+                        child: GestureDetector(
+                          onTap: () {
+                            productAddProvider.setProductCategory(category.id);
+                          },
+
+                          child: Chip(
+                            backgroundColor: isSelected
+                                ? Colors.deepPurple.shade100
+                                : Colors.grey.shade200,
+                            avatar: Icon(category.icon),
+                            label: Text(category.name),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  CustomTextField(
+                    labelText: 'Price',
+                    keyboardType: TextInputType.number,
+                    prefixText: 'LKR ',
+                    controller: productAddProvider.priceController,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Product Category'),
+                        FilledButton(
+                          onPressed: () {
+                            productAddProvider.addImages();
+                          },
+                          child: Text('Add Images'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
